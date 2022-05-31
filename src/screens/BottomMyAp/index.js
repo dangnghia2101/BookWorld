ae201f98ad8efd1d62cb2c635cba56e68713e870
dangnghia21 <nghiadtps16665@fpt.edu.vn>
2022-05-31 20:49:59 +0700

    Update

--- src/screens/BottomMyAp/index.js
+++ src/screens/BottomMyAp/index.js
@@ -1,12 +1,10 @@
 import HomeMyAp from './HomeMyAp';
 import ReadingScreenMyAp from './ReadingMyAp';
 import MoreScreenMyApp from './MoreMyApp';
-import InformMyAp from './InformMyApp';
 import ChatMyAp from './ChatMyAp';
 export const bottom = {
   HOME_MY_AP: HomeMyAp,
   READING_MY_APP: ReadingScreenMyAp,
   MORE_MY_APP: MoreScreenMyApp,
-  INFORM_MY_AP: InformMyAp,
   CHAT_MY_AP: ChatMyAp,
 };
