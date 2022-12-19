8d14b8871b9986df99b5b5c13b2a06095912ad81
dangnghia2101 <75264221+dangnghia2101@users.noreply.github.com>
2022-12-20 00:06:44 +0700

    fix (UI)

--- src/screens/BottomMyAp/MoreMyApp/MoreScreenMyApp/components/ItemMoreMy.js
+++ src/screens/BottomMyAp/MoreMyApp/MoreScreenMyApp/components/ItemMoreMy.js
@@ -1,39 +1,72 @@
 import { Block, Text } from '@components';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { makeStyles, useTheme } from 'themeNew';
 import { withNamespaces } from 'react-i18next';
 import { useAppSelector, useAppDispatch } from '@hooks';
+import { useGetReadTimeBookQuery } from '@redux/servicesNew';
 
 const ItemMoreMy = ({ props, t }) => {
     const themeStore = useAppSelector(state => state.root.themeApp.theme);
     const themeNew = useTheme(themeStore);
     const styles = useStyle(props, themeStore);
+    const [sumTimeRead, setSumTimeRead] = useState(0);
+    const myInfo = useAppSelector(state => state.root.auth);
+    const { data: dataReadTime } = useGetReadTimeBookQuery(myInfo._id);
+
+    useEffect(() => {
+        try {
+            if (dataReadTime) {
+                let handleData = [];
+
+                for (const year in dataReadTime) {
+                    let sum = 0;
+                    for (const month in dataReadTime[year]) {
+                        for (const day in dataReadTime[year][month]) {
+                            sum += dataReadTime[year][month][day];
+                        }
+                    }
+                    setSumTimeRead((sum / 60 / 60).toFixed(0));
+                }
+            }
+        } catch (e) {
+            console.log('[Error handleDataMonth] ', e);
+        }
+    }, []);
+
     return (
-        <Block
-            marginTop={10}
-            row
-            justifyContent={'space-around'}>
+        <Block marginTop={10} row justifyContent={'space-around'}>
             <Block style={[styles.dataContainer, styles.shadowColor]}>
-                <Text fontType={'medium1'} center color={themeNew.colors.textDark}>
+                <Text
+                    fontType={'medium1'}
+                    center
+                    color={themeNew.colors.textDark}>
                     {t('totalReadingTime')}
                 </Text>
                 <Text size={40} center color={themeNew.colors.textDark}>
-                    1280
+                    {sumTimeRead}
                 </Text>
-                <Text fontType={'medium1'} center color={themeNew.colors.textDark}>
-                    {t('minute')}
+                <Text
+                    fontType={'medium1'}
+                    center
+                    color={themeNew.colors.textDark}>
+                    {t('hours')}
                 </Text>
             </Block>
             <Block style={[styles.dataContainer, styles.shadowColor]}>
-                <Text fontType={'medium1'} center color={themeNew.colors.textDark}>
+                <Text
+                    fontType={'medium1'}
+                    center
+                    color={themeNew.colors.textDark}>
                     {t('numberOfBooksRead')}
-
                 </Text>
                 <Text size={40} center color={themeNew.colors.textDark}>
                     23
                 </Text>
-                <Text fontType={'medium1'} center color={themeNew.colors.textDark}>
+                <Text
+                    fontType={'medium1'}
+                    center
+                    color={themeNew.colors.textDark}>
                     {t('book')}
                 </Text>
             </Block>
